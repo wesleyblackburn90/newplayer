7b6f691bb1fb74e1386522eb9078c177f5f8cc33
Wesley Blackburn <wesley.blackburn90@yahoo.com>
2022-08-08 12:59:26 -0400

    testing heroku

--- react-app/src/components/Reviews/SingleReview.js
+++ react-app/src/components/Reviews/SingleReview.js
@@ -1,24 +1,35 @@
 import React, { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { deleteReviewThunk, getReviewsThunk } from '../../store/review'
+import { useHistory } from "react-router-dom"
 import EditReviewFormModal from "./EditReviewFormModal"
 
 function SingleReview({ singleReview, userId }) {
+  const history = useHistory()
   const dispatch = useDispatch()
   const sessionUser = useSelector((state) => state.session.user)
 
-  const deleteHandler = async () => {
-    await dispatch(deleteReviewThunk(singleReview))
+  const handleDelete = async (e) => {
+    e.preventDefault()
+    await dispatch(deleteReviewThunk(singleReview)).then(history.push(`/users/${sessionUser.id}`))
   }
 
   return (
     <div>
-      {singleReview &&
-        <div>
-          <h1>Rating: {singleReview.rating}</h1>
-          <h1>Review: {singleReview.comment}</h1>
-          <EditReviewFormModal singleReview={singleReview} profileId={userId} />
-        </div>
+      {singleReview && singleReview.reviewer_id === sessionUser.id ?
+        (
+          <div>
+            <h1>Rating: Hello{singleReview.rating}</h1>
+            <h1>Review: {singleReview.comment}</h1>
+            <EditReviewFormModal singleReview={singleReview} profileId={userId} />
+            <button onClick={handleDelete}>Delete</button>
+          </div>
+        ) : (
+          <div>
+            <h1>Rating: Hello{singleReview.rating}</h1>
+            <h1>Review: {singleReview.comment}</h1>
+          </div>
+        )
       }
     </div>
   )
