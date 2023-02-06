7aa8a2212f350e9c0c990b44fc560d0801868432
Wesley Blackburn <wesley.blackburn90@yahoo.com>
2023-02-06 13:53:07 -0500

    added undo seed commands

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .reviews import seed_reviews, undo_reviews
 from .sessions import seed_sessions, undo_sessions
+from .db import environment
 # from .games import seed_games, undo_games
 
 # Creates a seed group to hold our commands
@@ -12,6 +13,10 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        undo_users()
+        undo_reviews()
+        undo_sessions()
     seed_users()
     seed_reviews()
     seed_sessions()
