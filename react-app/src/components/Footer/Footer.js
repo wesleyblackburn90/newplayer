bae5a8b8756ab4e3bc2d51de3043eecb047b763a
Wesley Blackburn <wesley.blackburn90@yahoo.com>
2024-01-23 11:42:13 -0500

    upgraded pip

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div id="footer-wrap">
       <div id="footer-div">
-        <h1>About Wes</h1>
+        <h1>About Wesley</h1>
         <a target="_blank" href="https://www.linkedin.com/in/wesley-blackburn-333286232/">
           <img className="about-me-links" src="/static/linkedin.png"></img>
         </a>
